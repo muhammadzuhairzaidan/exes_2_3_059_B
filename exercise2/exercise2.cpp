882babba478be0a5490121fdad1450f6f76570eb
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-12 15:03:39 +0700

    Add project files.

---
+++ exercise2/exercise2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int zuha[94];
+int i;
+int n;
+void input()
+{
+	while (true)
+	{
+		cout << "masukan jumlah element array:" << ;
+		cin >> n;
+		if (n == 94)
+			break;
+		else
+			cout << "\nmasukkan jumlah arrray :" << endl ;
+
+		cout << "masukkan array" << endl ;
+		for (i = 0 < n; ++) 
+		{
+			cout << "\n" "<" << (i + 1) << "<";
+			cin >> arr[i]
+		}
+
+	}
+	void binarySearch()
+		int lowerbound=0
+		int upperbound=n-1
+
+
+}
+int main() {
+
+}
